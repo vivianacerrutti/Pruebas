10c2b6d03456acb1ace6af2df58f70e57f841387
Viviana Cerrutti <viviana.cerrutti@hotmail.com>
2013-02-19 17:42:01 -0200

    URL Limpia
    Se configuró la aplicación para que las URL aparecieran limpias

---
+++ protected/components/UserIdentity.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * UserIdentity represents the data needed to identity a user.
+ * It contains the authentication method that checks if the provided
+ * data can identity the user.
+ */
+class UserIdentity extends CUserIdentity {
+
+    private $_id;
+
+    public function authenticate() {
+        
+       /*
+        $record = User::model()->findByAttributes(array('username' => $this->username));
+        if ($record === null)
+            $this->errorCode = self::ERROR_USERNAME_INVALID;
+        else if ($record->password !== $this->password) //crypt($this->password, $record->password))
+            $this->errorCode = self::ERROR_PASSWORD_INVALID;
+        else {
+            $this->_id = $record->id;
+            $this->setState('title', $record->title);
+            $this->errorCode = self::ERROR_NONE;
+        }
+        return !$this->errorCode;
+        */
+        $this->_id=1;
+        return !self::ERROR_NONE;
+    }
+
+    public function getId() {
+        return $this->_id;
+    }
+
+}
